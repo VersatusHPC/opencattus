d2dd81fe196370b79ab189781d23060c42431998
Vinícius Ferrão <2031761+viniciusferrao@users.noreply.github.com>
2026-08-04 21:35:19 -0300

    Bump version to 1.2.0

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ from conan.tools.cmake import cmake_layout, CMakeToolchain, CMake
 
 class MyProjectConan(ConanFile):
     name = "OpenCATTUS"
-    version = "1.1.1"
+    version = "1.2.0"
     settings = "os", "arch", "compiler", "build_type"
 
     def requirements(self):
